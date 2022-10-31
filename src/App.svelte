2672bb03f090f944f867d55c82800e843f6563d2
MATHAVAN <mathavanselva495@gmail.com>
2022-10-31 13:08:22 +0530

    initial setup made and tailwind integrated

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<main>
+  <h1 class="text-center text-7xl text-orange-500">Hello World</h1>
+</main>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
